2a945873c4384dd345d2c178735c3806d2681467
mickeySTRANGE <spark2065871439@yahoo.co.jp>
2023-09-01 22:33:07 +0900

    get version auto

--- docs/js/application.js
+++ docs/js/application.js
@@ -3,6 +3,42 @@ function onloadFunc() {
     const fontClassList = ["font-aboshi", "font-cherryBomb", "font-chokokutai", "font-delaGothic"];
     let fontClass = fontClassList[Math.floor(Math.random() * fontClassList.length)];
     document.getElementById("pageTitle").classList.add(fontClass);
+
+    setAppVersion();
+}
+
+function setAppVersion() {
+    const versionUrlList = [
+        {
+            url: "https://mushi-deck-simulator-ef2b6bec2ae8.herokuapp.com/GetApplicationVersion",
+            id: "mushiDeckSimulatorVersion"
+        },
+        {
+            url: "https://new-book-notice.herokuapp.com/GetApplicationVersion",
+            id: "newBookNoticeVersion"
+        }
+    ];
+
+    versionUrlList.forEach(element => {
+        const request = new Request(
+            element.url,
+            {
+                method: "GET"
+            });
+        fetch(request).then(
+            function (response) {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                response.text().then(
+                    function (responseText) {
+                        document.getElementById(element.id).innerText = "v" + responseText;
+                    }
+                );
+            }
+        );
+    });
+
 }
 
 if (document.readyState === 'complete') {
